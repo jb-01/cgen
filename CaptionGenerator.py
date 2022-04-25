f674cfd16d166fdd0a14d339e29e04f3a3110529
jb-01 <joshuabelofsky@gmail.com>
2022-04-24 19:57:22 -0500

    LTSM

---
+++ CaptionGenerator.py
@@ -0,0 +1,28 @@
+import VGG19
+from keras import layers
+from keras.layers import Input, Flatten, Dropout, Activation
+from keras.layers.advanced_activations import LeakyReLU, PReLU
+# print(vocab_size)
+## image feature
+
+dim_embedding = 64
+
+input_image = layers.Input(shape=(Ximage_train.shape[1],))
+fimage = layers.Dense(256,activation='relu',name="ImageFeature")(input_image)
+## sequence model
+input_txt = layers.Input(shape=(maxlen,))
+ftxt = layers.Embedding(vocab_size,dim_embedding, mask_zero=True)(input_txt)
+ftxt = layers.LSTM(256,name="CaptionFeature",return_sequences=True)(ftxt)
+#,return_sequences=True
+#,activation='relu'
+se2 = Dropout(0.04)(ftxt)
+ftxt = layers.LSTM(256,name="CaptionFeature2")(se2)
+## combined model for decoder
+decoder = layers.add([ftxt,fimage])
+decoder = layers.Dense(256,activation='relu')(decoder)
+output = layers.Dense(vocab_size,activation='softmax')(decoder)
+model = models.Model(inputs=[input_image, input_txt],outputs=output)
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+print(model.summary())
